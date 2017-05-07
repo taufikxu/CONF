411ec92a40c768e22ee40575ae525cd077c9fcc6
taufikxuthu <kunxu.thu@gmail.com>
2017-05-07 22:22:53 +0800

    add bin, adjust conf_servo.sh

--- others/for_install/conf_servo.sh
+++ others/for_install/conf_servo.sh
@@ -19,6 +19,7 @@ ln -s $LN_HOME/jupyter ~/.jupyter
 ln -s $LN_HOME/.ssh ~/.ssh
 cp $LN_HOME/others/dircolors ~/.dircolors
 cp $LN_HOME/zshrc/zshrc_loader ~/.zshrc
+cp $LN_HOME/zshrc/chao.zsh-theme $LN_HOME/zshrc/oh-my-zsh/themes/
 
 
 ln -s $LN_HOME/tmux/tmux.theme.conf ~/.tmux.theme.conf
