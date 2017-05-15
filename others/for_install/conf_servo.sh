8a335d89ee87b308be7f5e549d5f000d358f292b
taufikxuthu <kunxu.thu@gmail.com>
2017-05-15 23:05:14 +0800

    remove .ssh

--- others/for_install/conf_servo.sh
+++ others/for_install/conf_servo.sh
@@ -8,14 +8,14 @@ rm ~/.tmux.conf
 rm ~/.tmux.theme.conf
 rm ~/.dircolors
 rm -rf ~/.oh-my-zsh
-rm -rf ~/.ssh
+# rm -rf ~/.ssh
 
 
 ln -s /mfs/kunxu/ ~/mfs
 ln -s $LN_HOME/vim ~/.vim
 ln -s $LN_HOME/vim/vimrc ~/.vimrc
 # git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s /home/kunxu/mfs/ENV/.ssh ~/.ssh
+# ln -s /home/kunxu/mfs/ENV/.ssh ~/.ssh
 cp $LN_HOME/others/dircolors ~/.dircolors
 cp $LN_HOME/zshrc/zshrc_loader ~/.zshrc
 # git clone https://github.com/robbyrussell/oh-my-zsh.git $LN_HOME/zshrc/oh-my-zsh
