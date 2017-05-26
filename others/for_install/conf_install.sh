3b7280764f8e21acfc3c4d6978332728a0b51fb1
kunxu <kunxu.thu@gmail.com>
2017-05-26 10:50:21 +0800

    optimize zshrc_loader

--- others/for_install/conf_install.sh
+++ others/for_install/conf_install.sh
@@ -1,4 +1,5 @@
 LN_HOME=/home/kunxu/CONF
+instance=local
 
 rm ~/mfs
 rm -rf ~/.vim 
@@ -14,13 +15,11 @@ rm -rf ~/.oh-my-zsh
 ln -s /mfs/kunxu/ ~/mfs
 ln -s $LN_HOME/vim ~/.vim
 ln -s $LN_HOME/vim/vimrc ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # ln -s /home/kunxu/mfs/ENV/.ssh ~/.ssh
 cp $LN_HOME/others/dircolors ~/.dircolors
 cp $LN_HOME/zshrc/zshrc_loader ~/.zshrc
-git clone https://github.com/robbyrussell/oh-my-zsh.git $LN_HOME/zshrc/oh-my-zsh
-cp $LN_HOME/zshrc/chao.zsh-theme $LN_HOME/zshrc/oh-my-zsh/themes/
-
+# git clone https://github.com/robbyrussell/oh-my-zsh.git $LN_HOME/zshrc/oh-my-zsh
 
 ln -s $LN_HOME/tmux/tmux.theme.conf ~/.tmux.theme.conf
 ln -s $LN_HOME/tmux/tmux.conf ~/.tmux.conf
--- others/for_install/tensorflow_python3.sh
+++
@@ -1,4 +0,0 @@
-export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-0.12.1-cp35-cp35m-linux_x86_64.whl
-
-pip install --upgrade $TF_BINARY_URL
-pip install --upgrade $TF_BINARY_URL --ignore-installed
