837a6a1d6e40c3c21ace9d0d347ac4b8f87b8fa5
taufikxuthu <kunxu.thu@gmail.com>
2017-06-14 11:31:51 +0800

    update theano install and rc

--- conda/theano.sh
+++ conda/theano.sh
@@ -4,5 +4,6 @@ which pip
 
 conda install jupyter numpy scipy scikit-learn matplotlib ipython
 pip install tqdm pillow path.py coloredlogs
-pip install theano
+# pip install theano
+pip install git+https://github.com/Theano/Theano.git#egg=Theano
 pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
