db8e522c4d3e9630b5b8760001480d75b6c89b2c
taufikxuthu <kunxu.thu@gmail.com>
2017-06-18 15:02:39 +0800

    add opencv

--- conda/base.sh
+++ conda/base.sh
@@ -2,4 +2,4 @@
 #
 which pip
 conda install jupyter numpy scipy scikit-learn matplotlib ipython pygpu
-pip install tqdm pillow path.py coloredlogs
+pip install tqdm pillow path.py coloredlogs opencv-python
