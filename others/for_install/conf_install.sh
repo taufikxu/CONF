f3fea63c6603f21c5a0b057bd0f2b85a2c8a335e
taufikxu <kunxu.thu@gmail.com>
2017-07-12 21:00:40 +0800

    update deepin's pycharm setting

--- others/for_install/conf_install.sh
+++ others/for_install/conf_install.sh
@@ -1,4 +1,4 @@
-LN_HOME=/home/taufikxu/CONF
+LN_HOME=/home/kunxu/Documents/CONF
 
 rm -rf ~/.vim 
 rm ~/.vimrc
@@ -12,11 +12,11 @@ rm -rf ~/.oh-my-zsh
 
 ln -s $LN_HOME/vim ~/.vim
 ln -s $LN_HOME/vim/vimrc ~/.vimrc
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # ln -s /home/kunxu/mfs/ENV/.ssh ~/.ssh
 cp $LN_HOME/others/dircolors ~/.dircolors
 cp $LN_HOME/zshrc/zshrc_loader ~/.zshrc
-# git clone https://github.com/robbyrussell/oh-my-zsh.git $LN_HOME/zshrc/oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git $LN_HOME/zshrc/oh-my-zsh
 
 ln -s $LN_HOME/tmux/tmux.theme.conf ~/.tmux.theme.conf
 ln -s $LN_HOME/tmux/tmux.conf ~/.tmux.conf
