ae5431617d93b4bcccf8d35447ad4933208d0f7a
taufikxuthu <kunxu.thu@gmail.com>
2018-01-26 13:06:46 +0800

    update tmux

--- conda/base.sh
+++ conda/base.sh
@@ -2,4 +2,4 @@
 #
 which pip
 conda install mkl jupyter numpy scipy scikit-learn matplotlib ipython pygpu
-pip install tqdm pillow path.py coloredlogs opencv-python
+pip install tqdm pillow path.py coloredlogs opencv-python h5py lmdb
