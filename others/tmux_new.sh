39a412bc339f02791f1ffba254215c039c110f3e
taufikxu <kunxu.thu@gmail.com>
2018-04-01 16:42:27 +0800

    update tmux and sync

--- others/tmux_new.sh
+++ others/tmux_new.sh
@@ -9,20 +9,13 @@ else
 fi
 
 
-FILE=/mfs/kunxu/ENV/TMUX.list
-LOG $(hostname -s):$session >> $FILE
-lines=$(wc -l < $FILE)
-if [ "$lines" -gt 1000 ]; then
-    sed -i "1d" $FILE;
-fi
-
 $cmd has -t $session 2> /dev/null
 
 if [ $? -ne 0 ]; then
-    $rcmd new -d -n nviw -s $session "watch nvidia-smi"
-    $rcmd split-window -h -t $session:0 "watch nvidia-who"
+    $rcmd new -d -n nviw -s $session "watch gpustat"
+    $rcmd split-window -h -t $session:0
     #$rcmd split-window -v -t $session:0.0 "watch nvidia-who"
-    $rcmd resize-pane -t $session:0.0 -x 35
+    $rcmd resize-pane -t $session:0.0 -x 85
     $rcmd neww -n zsh -t $session "zsh"
 fi
 
