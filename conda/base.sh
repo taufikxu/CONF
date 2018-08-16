9a38556c374991b28e0762af75fb764a4dbb7def
taufikxuthu <kunxu.thu@gmail.com>
2018-08-16 21:01:53 +0800

    add update pip to shell scipt for conda

--- conda/base.sh
+++ conda/base.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 #
 which pip
+pip install --upgrade pip
 conda install mkl jupyter numpy scipy scikit-learn matplotlib ipython pygpu
 pip install tqdm pillow path.py coloredlogs opencv-python h5py lmdb networkx cython flake8 autopep8 yapf 
 pip install imageio scikit-image seaborn statsmodels
--- conda/theano.sh
+++ conda/theano.sh
@@ -2,6 +2,7 @@
 #
 which pip
 
+pip install --upgrade pip
 conda install jupyter numpy scipy scikit-learn matplotlib ipython
 pip install tqdm pillow path.py coloredlogs
 # pip install theano
