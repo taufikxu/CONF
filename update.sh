c377f2861cc627b91851477bceb0cc9fd9e3c66f
taufikxu <kunxu.thu@gmail.com>
2018-12-03 17:16:17 +0800

    remove workspace

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ MPATH="/home/kunxu/"
 
 rsync -azv --delete jungpu$1:$MPATH/ENV/ $MPATH/ENV/
 rsync -azv --delete jungpu$1:$MPATH/CONF/ $MPATH/CONF/
-rsync -azv --delete jungpu$1:$MPATH/workspace/ $MPATH/workspace/
+# rsync -azv --delete jungpu$1:$MPATH/workspace/ $MPATH/workspace/
