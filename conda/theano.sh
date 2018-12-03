e8b81fbd639404c1e2d6857cd59bb23fb6a89f79
taufikxu <kunxu.thu@gmail.com>
2018-12-03 17:01:20 +0800

    add workspace

--- conda/theano.sh
+++ conda/theano.sh
@@ -5,6 +5,6 @@ which pip
 pip install --upgrade pip
 conda install jupyter numpy scipy scikit-learn matplotlib ipython
 pip install tqdm pillow path.py coloredlogs
-# pip install theano
-pip install git+https://github.com/Theano/Theano.git#egg=Theano
+pip install theano
+# pip install git+https://github.com/Theano/Theano.git#egg=Theano
 pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
